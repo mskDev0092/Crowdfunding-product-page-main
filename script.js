71c2946512ba5f1b701f54480ba37efed2a76493
Shehzad Khan <102049709+mskDev0092@users.noreply.github.com>
2023-03-09 18:09:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+//localStorage.removeItem("totalAmount");
+//localStorage.removeItem("backers");
+//localStorage.removeItem("bamboo");
+//localStorage.removeItem("black");
+
+/* ----------------------- [on load] ------------------------------*/
+
+//Retrieve info from local storage
+
+// Retrieve total amount
+
+// check if NaN value
+
+// Retrieve backers
+
+//Retrieve stock left
+
+//document.querySelector('')
+
+// Progress bar style on load
+
+/*--------------- [click toggle button ] ------------------- */
+
+/*--------------- [increment total amounts & backer] -------------- */
+
+//Fetch amount from imput
+
+//Recalculate index
+
+//Fetch minium amount
+
+//Fetch default value if no amount is entered
+
+// Check if value is above min & increment
+
+// Get data attribute for stock level
+
+// Update stock info if stock is above 0
+
+//Update all relevant stock items
+
+//update value in webAPI
+
+//Update all relevant stock items
+
+//update value in webAPI
+
+//==> page refresh and out of stock items dealt with in webAPI
+
+/*----------------- [update total money raised] ------------------*/
+/*progress bar*/
+// Update local storage
+/*------------------ [incrementation backers by 1] -----------------*/
+//Update local storage
+/*------------------- [toggle whether product is bookmarked] ------------------*/
+/*https://redstapler.co/toggle-active-button-state-javascript/*/
+/*------------------- [success modal open/close] ------------------*/
+
+/*------------------------- [countdown] -----------------------------*/
+
+//Calculate days
+//Calculate hours and seconds
+//Fetch the right value according to distance left
+//Upadate HTML
+//Once finished
+
+/*----------------------- [ modals ] ---------------------------*/
+
+/*-- FAO Mike: Kelv's additional modal scripts - to be checked ---*/
+// Get the modal
+
+// Get the button that opens the modal
+
+// When the user clicks the button, open the modal
+
+// Close modal
+
+// Get the <span> element that closes the modal
+
+// When the user clicks on <span> (x), close the modal
+
+// When the user clicks anywhere outside of the modal, close it
+
+// Success modal
